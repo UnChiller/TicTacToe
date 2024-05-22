255f22195ae618620478be3c9a77bf1fd3f17dbd
UnChiller <81498434+UnChiller@users.noreply.github.com>
2024-05-22 11:36:21 -0500

    Add files via upload

---
+++ ttt.ts
@@ -0,0 +1,17 @@
+class TicTacToeBoard {
+    /*
+        0: blank
+        1: X
+        2: O
+    */
+    private myBoard: number[][] = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    constructor(state?: number[][]) {
+        if (state) {
+            
+        }
+    }
+}
